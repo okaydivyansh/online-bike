4eaaba8e6ef973922a3635cb9048d7058798f168
Divyansh Mishra <divyanshm09@example.com>
2023-11-16 02:12:54 +0530

    yes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-      Footer
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+          <Link
+            to="/"
+            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          ></Link>
+          <span class="text-muted">© 2023 Bike Room, Inc</span>
+        </div>
+      </footer>
     </div>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import NavigationBar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import NavigationBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Corousel from "../components/Corousel";
 const Home = () => {
   return (
     <div>
-      <div> <NavigationBar/></div>
-      <div><div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="...">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-      <div> <Footer/></div>
+      <div>
+        <NavigationBar />
+      </div>
+      <div>
+        <Corousel />
+      </div>
+      <div>
+        <Card />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
